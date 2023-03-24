e4157477da5082730684ab42bb4ae18ff2e9f8c2
Robert Arroyos <robertarroyosjr@gmail.com>
2023-03-24 16:43:23 -0500

    fixed a minor typo

--- data_1.py
+++ data_1.py
@@ -3,7 +3,7 @@ from sklearn.datasets import load_iris
 # I also found Scikit-learn has a "preprocessing" module.  Here, I import it from the sklearn library.
 from sklearn import preprocessing
 
-# I store the "load_iris" dataset into a veriable "data#
+# I store the "load_iris" dataset into a variable "data#
 data = load_iris()
 
 # I extract the input features from the dataset and store them in a variable "x_data"
